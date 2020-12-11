6a12250886a04d01f1b737824910d730216d292c
Kids <Kids@DESKTOP-7FR8S53>
2020-12-11 15:41:51 -0500

    Zeb Alien Code

---
+++ zeb/Aliens/alien_invastion.py
@@ -0,0 +1,79 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+from bullet import Bullet
+
+class AlienInvasion:
+    def __init__(self):
+        pygame.init()
+        
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        #self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        #self.settings.screen_width = self.screen.get_rect().width
+        #self.settings.screen_height = self.screen.get_rect().height
+        
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+
+    def run_game(self):
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_bullets()
+            print(len(self.bullets))
+            
+            self._update_screen()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit(0)
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+    
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+       
+    def _update_bullets(self):
+        self.bullets.update()
+            
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+               self.bullets.remove(bullet)
+        print(len(self.bullets))
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)           
+        self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ zeb/Aliens/settings.py
@@ -0,0 +1,19 @@
+class Settings:
+	"""A class to store all settings for Alien Invaion."""
+	def __init__(self):
+		"""Intiialize the game's settings."""
+
+		#Screen Settings
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (230, 230, 230,)
+
+		#Ship Settings
+		self.ship_speed = 1.5
+
+		#Bullet Settings
+		self.bullet_speed = 1.0
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = (60, 60, 60)
+		self.bullets_allowed = 55
