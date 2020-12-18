2d6a18afe2573dae95fba1e1f8ad086669efb781
Bradley Weldy <bradleyweldy@Bradleys-MBP.attlocal.net>
2020-12-17 20:53:33 -0500

    Aliens

--- shep/Aliens/settings.py
+++ shep/Aliens/settings.py
@@ -7,8 +7,20 @@ class Settings:
         # Screen settings 
         self.screen_width = 1200
         self.screen_height = 700
-        self.bg_color = (0, 0, 225)
+        self.bg_color = (230, 230, 230)
 
         self.ship_speed = 1.5
 
+        # Bullit settings
+        self.bullet_speed = 1.5
+        self.ship_limit = 3
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (0, 225, 0)
+
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
             
