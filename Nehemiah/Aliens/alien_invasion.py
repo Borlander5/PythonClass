7a5e1ed9ff8b286982f3c4650dcccc4c81d4207e
NMB01 <borlandfamily7@gmail.com>
2021-03-04 19:39:48 -0500

    hello there

--- Nehemiah/Aliens/alien_invasion.py
+++ Nehemiah/Aliens/alien_invasion.py
@@ -75,6 +75,8 @@ class AlienInvasion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.prep_ships()
+            self.sb.check_high_score()
        
         if not self.aliens:
             #Destroy existing bullets and create a new fleet.
@@ -218,7 +220,7 @@ class AlienInvasion:
         if self.stats.ships_left > 0:
             #Decrement ships left.
             self.stats.ships_left -= 1
-
+            self.sb.prep_ships()
             #Get rid of any remaining aliens and bullets.
             self.aliens.empty()
             self.bullets.empty()
--- Nehemiah/Aliens/scoreboard.py
+++ Nehemiah/Aliens/scoreboard.py
@@ -1,9 +1,13 @@
 import pygame.font
+from pygame.sprite import Group
+
+from ship import Ship
 
 
 class Scoreboard:
 
     def __init__(self, ai_game):
+        self.ai_game = ai_game
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
@@ -14,6 +18,7 @@ class Scoreboard:
 
         self.prep_score()
         self.prep_high_score()
+        self.prep_ships()
 
     def prep_score(self):
         rounded_score = round(self.stats.score, -1)
@@ -38,8 +43,17 @@ class Scoreboard:
             self.stats.high_score = self.stats.score
             self.prep_high_score()
 
+    def prep_ships(self):
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_game)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
+
     def show_score(self):
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.ships.draw(self.screen)
 
 
