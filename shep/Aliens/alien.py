dbf7d719f5abb5dfa8c657cd58df895e42f40482
Bradley Weldy <bradleyweldy@Bradleys-MBP.attlocal.net>
2021-03-12 15:09:01 -0500

    Some string

--- shep/Aliens/alien.py
+++ shep/Aliens/alien.py
@@ -15,20 +15,72 @@ class Alien(Sprite):
         # Start eaach new alien near the top left of the screen.
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
-        
+
         # Store the alien's exact horizontal position.
+        self.y = float(self.rect.y)
         self.x = float(self.rect.x)
+        self.path = 1
+        self.pathCount = 0
+        self.yDirection = 1
+        self.xDirection = 1
+        self.fake_bottom = 300
 
 
     def check_edges(self):
         """Return True if alien is at edge of screen."""
         screen_rect = self.screen.get_rect()
-        if self.rect.right >= screen_rect.right or self.rect.left <= -1:
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
             return True
 
     def update(self):
-        """Move the alien right or left."""
-        self.x += (self.settings.alien_speed *
-                        self.settings.fleet_direction)
+        self.x += self.xDirection * self.settings.alien_speed
+        self.y += self.yDirection * self.settings.alien_speed
+        self.fake_bottom += 0.1
+        screen_rect = self.screen.get_rect()
+        
+        if self.y >= self.fake_bottom or self.y <= 0:
+            self.yDirection *= -1
+        elif self.x >= screen_rect.right or self.x <= 0:
+            self.xDirection *= -1
+        
+                
         self.rect.x = self.x
+        self.rect.y = self.y
+
+"""
+    def update(self):
+        
+
+        if self.path == 1:
+            self.x -= self.settings.alien_speed
+            self.y /= self.settings.alien_speed
+        elif self.path == 2:
+            self.x += self.settings.alien_speed 
+            self.y *= self.settings.alien_speed 
+        elif self.path == 3:
+            self.x += self.settings.alien_speed
+            self.y -= self.settings.alien_speed
+        elif self.path == 4:
+            self.x *= self.settings.alien_speed
+            self.y += self.settings.alien_speed           
+        elif self.path == 5:
+            self.x += self.settings.alien_speed
+            self.y /= self.settings.alien_speed
+        elif self.path >= 6:
+            self.y += self.settings.alien_speed 
+
+        self.pathCount += 2
+        if self.pathCount >= 500:
+            self.path += 1
+            self.pathCount = 0
+
+
+
+            
+        self.rect.x = self.x
+        self.rect.y = self.y    
+        
+"""
+        
+        
     
--- shep/Aliens/alien_invasion.py
+++ shep/Aliens/alien_invasion.py
@@ -5,6 +5,8 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -23,7 +25,9 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         # Create an instance to store game statistics.
+        #   and create a scoreboard.
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -31,6 +35,8 @@ class AlienInvasion:
 
         self._create_alien_boss()
 
+        # Make the Play button.
+        self.play_button = Button(self, "Play")
         # Set the background color.
         self.bg_color = (230, 230, 230)
 
@@ -70,10 +76,22 @@ class AlienInvasion:
         # Remove any bullets and aliens that have collided.
         collisions = pygame.sprite.groupcollide(
                 self.bullets, self.aliens, True, True)
+
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+            self.sb.check_high_score()
+
+
+
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_alien_boss()
+            self.settings.increase_speed()
+
+        
 
    
 
@@ -86,6 +104,32 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the player clicks Play."""
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            #Reset the game settings.
+            self.settings.initialize_dynamic_settings()
+            # Reset the game statistics.
+            self.stats.reset_stats()
+            self.stats.game_active = True
+            self.sb.prep_score()
+
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
+
+            # Get rid of any remaining aliens and bullets.
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Create a new fleet and center the skip.
+            self._create_alien_boss()
+            self.ship.center_ship()
+
 
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
@@ -119,12 +163,22 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        # Draw the score information.
+        self.sb.show_score()
+
+        # Draw the play button if the game is inactive.
+        if not self.stats.game_active:
+            self.play_button.draw_button()
         
         # Make the most recently drawn screen visible.
         pygame.display.flip() 
 
-    def _create_alien_boss(self):        
-        self._create_alien(1, 1)
+    def _create_alien_boss(self): 
+        self._create_alien(0, 0)      
+        self._create_alien(4, 4)
+        self._create_alien(7, 7)
+        
 
     def _create_alien_grid(self):
         """Create the fleet of aliens."""
@@ -187,6 +241,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
     
 
     def _update_aliens(self):
